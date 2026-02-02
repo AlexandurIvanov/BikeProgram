e5f88cc63a16f0b2f38646c03119117c0c00bdd3
AlexandurIvanov <stu2213811049@uni-plovdiv.bg>
2026-02-02 15:45:38 +0200

    IBikeCrudService.cs

---
+++ BikeProgram.BL/Interfaces/IBikeCrudService.cs
@@ -0,0 +1,12 @@
+﻿using LibrarySystem.Models.Dto;
+
+namespace LibrarySystem.BL.Interfaces
+{
+    public interface IBookCrudService
+    {
+        List<Book> GetAllBooks();
+        Book? GetById(Guid id);
+        void AddBook(Book book);
+        void DeleteBook(Guid id);
+    }
+}
